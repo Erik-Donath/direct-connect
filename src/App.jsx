df72ee9a3627df4a9831fcd53af813c234965a30
Erik Donath <74675557+Erik-Donath@users.noreply.github.com>
2025-06-20 20:29:52 +0000

    Test PeerJS

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,53 @@
+import { useState } from 'react';
 import './App.css'
+import { Peer } from "peerjs";
+
+const peer = new Peer()
 
 function App() {
-  return <h1>Direct Connect</h1>
+  const [hostID, setHostID] = useState("Loading...")
+  const [clientID, setClientID] = useState('');
+
+  peer.on('open', id => {
+    setHostID(id);
+    console.log('Server peer ID:', id);
+  });
+
+  peer.on('connection', conn => {
+    console.log('Connected to:', conn.peer);
+	  conn.on('data', function(data) {
+	    console.log('Received', data);
+      conn.send(data);
+	  });
+  });
+
+  const handelSetClientID = (event) => {
+    setClientID(event.target.value);
+  }
+
+  const connectToHost = () => {
+    console.log("Host ID:", clientID);
+    const conn = peer.connect(clientID);
+    conn.on("open", () => {
+      conn.send("hi!");
+    });
+    conn.on("data", function(data) {
+	    console.log('Received', data);
+      conn.send(data);
+	  })
+  }
+
+  return <>
+    <h1>Connect to: {hostID}</h1>
+
+    <input
+      type="text"
+      value={clientID}
+      onChange={handelSetClientID}
+      placeholder='Type ID'
+      ></input>
+    <button onClick={connectToHost}>Connect to Host</button>
+  </>
 }
 
 export default App
