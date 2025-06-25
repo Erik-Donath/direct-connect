77a9c8b0aac226ac88d00a811d5fd74777c9c503
C42xe2 <erikdonath2006@gmail.com>
2025-06-25 10:25:18 +0200

    Fixed Typo

--- src/main.jsx
+++ src/main.jsx
@@ -5,21 +5,23 @@ import './index.css';
 
 if ('serviceWorker' in navigator && import.meta.env.PROD) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('service-worker.js').then(reg => {
-      reg.onupdatefound = () => {
-        const newWorker = reg.installing;
-        newWorker.onstatechange = () => {
-          if (
-            newWorker.state === 'activated' &&
-            navigator.serviceWorker.controller
-          ) {
-            window.location.reload();
-          }
+    navigator.serviceWorker.register('service-worker.js')
+      .then(reg => {
+        reg.onupdatefound = () => {
+          const newWorker = reg.installing;
+          newWorker.onstatechange = () => {
+            if (
+              newWorker.state === 'activated' &&
+              navigator.serviceWorker.controller
+            ) {
+              window.location.reload();
+            }
+          };
         };
-      };
-    });
-  }).catch(error => {
-    console.warn('Service worker registration failed:', error);
+      })
+      .catch(error => {
+        console.warn('Service worker registration failed:', error);
+      });
   });
 }
 
